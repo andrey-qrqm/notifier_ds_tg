22d72d56d009c6c2e5108dbeac418b9748ca8d09
Andrey QRQM <andrey.madyarov@gmail.com>
2025-01-08 13:15:19 +0300

    add tests for notifier_tg

--- test_notifier_tg.py
+++ test_notifier_tg.py
@@ -8,15 +8,18 @@ def test_remove_spaces():
     assert remove_spaces("   ") == ""
     assert remove_spaces("") == ""
 
+
 def test_extract_arg():
     assert extract_arg("/add_channel test_channel") == ["test_channel"]
     assert extract_arg("") == []
+
 
 @patch('psycopg2.connect')
 def test_conn_check(mock_connect):
     mock_connect.return_value = MagicMock()
     conn_check()
     mock_connect.assert_called_once()
+
 
 @patch('psycopg2.connect')
 def test_create_database_conn(mock_connect):
