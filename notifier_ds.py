137c49052375040a1943a0589ddcc0ae054346aa
Andrey QRQM <andrey.madyarov@gmail.com>
2025-09-08 14:41:03 +0300

    logging for topics

--- notifier_ds.py
+++ notifier_ds.py
@@ -92,6 +92,7 @@ def send_data(event_msg, url, discord_channel_name, conn, event_id, is_join, dat
                 'disable_notification': True
             }
         else:
+            logging.info(f"chat id: {int(tg_id)}, Required for topics: {CHAT_ID}, topic = {TOPIC_ID}")
             data = {
                 'chat_id': int(tg_id),
                 'topic_id': None,
