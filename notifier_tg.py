59680dcadd9014a7026068ed0b3609b3d938b09f
Andrei M <quberq@LAPTOP-91VRVFSU.>
2026-05-07 18:33:02 +0300

    add round(sum()) to get_sessions

--- notifier_tg.py
+++ notifier_tg.py
@@ -214,7 +214,7 @@ async def get_sessions(message):
 
     cur.execute("""
         SELECT user_id,
-            EXTRACT(EPOCH FROM (session_end - session_start)) / 60 AS duration_minutes
+            ROUND(SUM(EXTRACT(EPOCH FROM (session_end - session_start)) / 60))::INT AS duration_minutes
         FROM discord_sessions
         WHERE DISCORD_ID = %s AND session_end IS NOT NULL
         GROUP BY user_id
